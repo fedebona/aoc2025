095be3a72bc6f2cba684754cc6edfca8cef50d5d
Federico Bonarrigo <federico.bonarrigo@apparound.com>
2025-12-04 01:34:10 +0100

    completed day 1

---
+++ day01.py
@@ -0,0 +1,53 @@
+# Advent of Code 2025 - Day 1
+def P1(f):
+    current = 50
+    zeros = 0
+    for line in f.readlines():
+        if line[0] == 'R':
+            increment = int(line[1:])
+            current = Add(current, increment)
+        elif line[0] == 'L':
+            decrement = int(line[1:])
+            current = Subtract(current, decrement)
+        if current == 0:
+            zeros += 1
+    return zeros
+
+
+def P2(f):
+    current = 50
+    zeros = 0
+    for line in f.readlines():
+        if line[0] == 'R':
+            increment = int(line[1:])
+            zeros += RoundsR(current, increment)
+            current = Add(current, increment)
+        elif line[0] == 'L':
+            decrement = int(line[1:])
+            zeros += RoundsL(current, decrement)
+            current = Subtract(current, decrement)         
+    return zeros
+
+
+def Add(current, increment):
+    return (current + increment) % 100
+
+def Subtract(current, decrement):
+    return (current - decrement + 100) % 100
+
+def RoundsR(current, increment):
+    return (current + increment) // 100
+
+def RoundsL(current, decrement):
+    newCurrent = Subtract(current, decrement)
+    loops = decrement // 100
+    if current == 0:
+        return loops
+    return loops + (1 if newCurrent >= current else 0) + (1 if newCurrent == 0 else 0)
+
+f = open("resources\day01.txt", "r")
+print(f"Day 1/1: {P1(f)}")
+f.close()
+f = open("resources\day01.txt", "r")
+print(f"Day 1/2: {P2(f)}")
+f.close()
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-#Advent of code 2025 fedebona Repo
+# Advent of code 2025 fedebona Repo
+
 This repository contains my solutions for the Advent of Code 2025 challenges. Each day's challenge is solved in a separate file, organized by day.
